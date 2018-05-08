2de2e33c283cf71d3b6e40690a0ebbe9977ff925
ucasstudent <student@mails.ucas.ac.cn>
2018-05-08 09:38:46 +0800

    Modified

--- Week2-Classification.py
+++ Week2-Classification.py
@@ -1,7 +1,7 @@
 import torch
+from torch.autograd import Variable
 import matplotlib.pyplot as plt
 import numpy as np
-from torch.autograd import Variable
 import torch.nn.functional as F
 
 n_data=torch.ones(100,2)
@@ -15,8 +15,44 @@ y=torch.cat((y0,y1),).type(torch.FloatTensor)
 
 x,y=Variable(x),Variable(y)
 
-plt.scatter(x.data.numpy()[:,0],x.data.numpy()[:,1],c=y.data.numpy(),s=100,lw=0,cmap='RdYlGn')
+plt.scatter(x.data.numpy()[:,0],x.data.numpy()[:,1],c=y.data.numpy(),s=100,cmap='RdYlGn')
+
+class Net(torch.nn.Module):
+    def __init__(self,n_feature,n_hidden,n_output):
+        super(Net,self).__init__()
+        self.hidden=torch.nn.Linear(n_feature,n_hidden)
+        self.output=torch.nn.Linear(n_hidden,n_output)
+    def forward(x):
+        x=F.relu(self.hidden(x))
+        x=self.output(x)
+        return x
+
+net=Net(n_feature=2,n_hidden=10,n_output=2)
+print(net)
+
+optimizer=torch.optim.SGD(net.parameters(),lr=0.02)
+
+loss_func=torch.nn.CrossEntropyLoss()
+
+plt.ion()
+
+for t in range(100):
+    out=net(x)
+    loss=loss_func(out,y)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if t%2==0:
+        # Show learning process
+        plt.cla()
+        prediction=torch.max(out,1)[1]
+        pred_y=prediction.data.numpy()
+        target_y=y.data.numpy()
+        plt.scatter(x.data.numpy()[:,0],x.data.numpy()[:,1],c=pred_y,s=100,lw=0,cmap='RdYlGn')
+        accuracy=sum(pred_y==target_y)/200
+        plt.text(1.5,-4,'Accuracy=%.2f' % accuracy, fontdict={'size':20,'color':'red'})
+        plt.pause(0.1)
+
+plt.ioff()
 plt.show()
-
-
-
